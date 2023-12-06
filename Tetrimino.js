327170797da65b4c26538293897903119c28a072
SantiagoHM27 <himejimadarkrai@gmail.com>
2023-12-06 11:40:43 -0600

    03 Ya casi

--- Tetrimino.js
+++ Tetrimino.js
@@ -57,6 +57,74 @@ class Tetrimino {
             pmino.set(-pmino.y, pmino.x);
         }
     }
+
+    get movimientoErroneo(){
+        let salióDelTablero = !this.estáDentroDelTablero;
+        return salióDelTablero || this.colisiónConMinosAlmacenados;
+    }
+
+    get colisiónConMinosAlmacenados() {
+        for (const pmino of this.mapaTablero) {
+            if (tablero.minosAlmacenados[pmino.x][pmino.y]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    get estáDentroDelTablero() {
+        for (const pmino of this.mapaTablero) {
+            if (pmino.x < 0) {
+                return true;
+            }
+            if (pmino.x >= tablero.columnas) {
+                return false;
+            }
+            if (pmino.y >= tablero.filas) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    get mapaTablero() {
+        let retorno = [];
+        for (const pmino of this.mapa) {
+            let copy = pmino.copy().add(this.posición);
+            retorno.push(copy);
+        }
+        return retorno;
+    }
+
+    dibujar() {
+        push();
+        fill(this.color);
+        for ( const pmino of this.mapaCanvas) {
+            Tetrimino.dibujarMino(pmino);
+        }
+        pop();
+        if ( tetrimino == this) {
+            this.dibujarEspectro();
+        }
+    }
+
+    dibujarEspectro(){
+        this.espectro = new Tetrimino(this.nombre);
+    this.espectro.posición = this.posición.copy()
+    for (let i = 0; i < this.mapa.length; i++) {
+      this.espectro.mapa[i] = this.mapa[i].copy()
+    }
+    while (this.espectro.moverAbajo());
+    push()
+    drawingContext.globalAlpha = 0.3
+    this.espectro.dibujar();
+    pop()
+
+    
+
+    }
+
+     
     
     
 }
